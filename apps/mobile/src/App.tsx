a95cdd8740ffb63da9e62e09b5fca5cc6fa4c3ce
Michael Trindade <michael.trindade@hotmail.com>
2024-07-23 13:23:39 -0400

    initialized core for rule configuration

---
+++ apps/mobile/src/App.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+import { somar } from "@barba/core";
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>App{somar(32, 43)}</Text>
+    </View>
+  );
+}
